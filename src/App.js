6f649edad1b51c62dd505fc239b3227547050795
jonathan2025 <Jonathan.Liang75@outlook.com>
2023-05-25 12:52:45 -0400

    created the skills section

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import NavBar from "./components/NavBar";
 import Home from "./components/Home";
 import About from "./components/About";
-
+import Skills from "./components/Skills";
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Home />
       <About />
+      <Skills />
     </div>
   );
 }
