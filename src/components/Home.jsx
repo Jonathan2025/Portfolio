3756e498e698a7e2ff2daf5c6394ea4f8d0932e4
jonathan2025 <Jonathan.Liang75@outlook.com>
2023-05-24 23:38:44 -0400

    so far finished with the home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,28 @@ import {HiArrowNarrowRight} from 'react-icons/hi'
 
 const Home = () => {
   return (
-    <div className="w-full h-screen bg-[#72a8d5]">
+    <div name="home" className="w-full h-screen bg-[#72a8d5]">
+
+        {/* container for all the elements in the home page, note "px" stands for padding*/}
+        <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+            <p className="text-white">Hey there, my name is </p>
+            {/* text will change size depending on size of the screen */}
+            <h1 className="text-4xl sm:text-7xl font-bold text-[white]">Jonathan Liang</h1>
+            <h2 className="text-4xl sm:text-7xl font-bold text-[gray]">Full Stack Software Engineer</h2>
+            {/* on larger screens this is so that the p tag doesnt take the full page */}
+            <p className ="text-[gray] py-4 max-w-[700px]">I am a full stack software engineer that specializes and loves to 
+              build full stack applications that empower users financially and physically.</p>
+
+            <div>
+                <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-orange-400  hover:border-orange-400'>View Work 
+                <span className='group-hover:rotate-90 duration-300'>
+                  <HiArrowNarrowRight className='ml-3'/>
+                </span>
+                </button>
+            </div>
+        </div>
+
+
 
     </div>
   )
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -45,7 +45,8 @@ const NavBar = () => {
         </ul>
 
         {/* social icons */}
-        <div className="flex fixed flex-col top-[35%] left-0"> 
+        {/* When the screen is too small the social icons will be hidden, otherwise they will show */}
+        <div className="hidden lg:flex fixed flex-col top-[35%] left-0"> 
             <ul>
                 {/* this creates a pop up of the linked in when you hover */}
                 <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[10px] duration-300 bg-blue-500'> 
