7053598fb2e526f298bc2a17b39ab24d895c0133
jonathan2025 <Jonathan.Liang75@outlook.com>
2023-05-25 11:28:34 -0400

    about section completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import NavBar from "./components/NavBar";
 import Home from "./components/Home";
+import About from "./components/About";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Home />
+      <About />
     </div>
   );
 }
---
+++ src/components/About.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div name="about" className='w-full h-screen bg-[green] text-gray-300'>
+        <div className='flex flex-col justify-center items-center w-full h-full'>
+            <div className='max-w-[1000px] w-full px-4 grid grid-cols-2 gap-8'>
+                {/* when the screen gets small, the text will jump to the right */}
+                <div className='sm:text-right pb-8 pl-4'>
+                    <p className='text-4xl font-bold inline border-b-4 border-blue-600'>
+                        About
+                    </p>
+                </div>
+                <div> </div>
+                {/* When the screen gets smaller then the text will become one column */}
+                <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4'>
+                    <div className='sm:text-right text-4xl font-bold'> 
+                        <p> Hey, I'm Jonathan nice to meet you please take a look around</p>
+                    </div>
+                </div>
+                <div>
+                    <p> 
+                        As a software engineer, I want to share my knowledge in investing and martial arts by building full stack applications 
+                        that help others achieve their financial and physical goals. I LOVE helping others achieve the better version of themselves.
+                    </p>
+                </div>
+
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default About
